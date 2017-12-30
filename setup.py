b2cbd108c8aef58d22b38405d8217bcb93f33b61
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2017-12-30 13:01:34 +0100

    Added setup file.

---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+import os
+from setuptools import setup, find_packages, Command
+
+def readme():
+    os.system("pandoc --from=markdown --to=rst --output=README.rst README.md")
+    with open('README.rst') as f:   # has to be in .rst format
+        return f.read()
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root"""
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        if os.name == "posix":
+            os.system(
+                'rm -vrf ./build ./dist ./*.pyc ./*tgz ./*.egg-info *.rst *.db *~'
+            )
+
+setup(name = 'remarkable-tweak',
+      version = '0.0.1',
+      description = 'Tweak tool for the reMarkable paper tablet.',
+      long_description = readme(),
+      classifiers = [
+          'Development Status :: 1 - Planning',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 3',
+          'Topic :: Utilities'
+      ],
+      url = 'https://github.com/morngrar/remarkable-tweak',
+      author = 'Svein-Kåre Bjørnsen',
+      author_email = 'sveinkare@gmail.com',
+      license = 'GPL',
+      include_package_data = True,
+      packages = find_packages(),
+      install_requires = [
+          "paramiko>=2.4.0",
+      ],
+      entry_points = {
+#          'console_scripts': [
+#              'remarkable-tweak = remarkable_tweak.commands:main'
+#          ]
+      },
+      cmdclass = {
+          'clean': CleanCommand,
+      },
+      zip_safe = False
+)
