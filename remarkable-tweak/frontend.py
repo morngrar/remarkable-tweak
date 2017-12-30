04e855bfbbc00ccf7ed8986ab3b226c64a753489
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2017-12-30 11:57:59 +0100

    Added ghost content.

---
+++ remarkable-tweak/frontend.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+
+# This file will contain the gui
---
+++ remarkable-tweak/ssh.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+# This file will handle the SSH-connection to the remarkable
+
+# this is already being worked on in separate branch
