6952be33c807d915b13e6e0e6669d7d76b790a79
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2018-01-08 16:45:57 +0100

    Implemented cleaning of working directory.

--- remarkable_tweak/backend.py
+++ remarkable_tweak/backend.py
@@ -3,10 +3,19 @@
 """Backend, separating ssh-code from frontend"""
 
 import ssh
+import os
+import shutil
+
 import system
 
 PASSWORD = ""
 
+def clean_working_dir():
+    for f in os.listdir(system.WORKING_DIR):
+        path = os.path.join(system.WORKING_DIR, f)
+
+        if os.path.isfile(path):
+            os.unlink(path)
 
 def download():
     connection = ssh.RemarkableConnection(PASSWORD)
@@ -16,3 +25,7 @@ def upload(path_list):
     connection = ssh.RemarkableConnection(PASSWORD)
     connection.purge_templates()
     connection.upload_templates(path_list)
+
+    clean_working_dir()
+
+
