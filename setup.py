e7fe8d31a3b4510d25855a9d18f8eaa37ffaf55e
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2018-01-30 11:19:41 +0100

    fixed indentation.

--- setup.py
+++ setup.py
@@ -21,34 +21,35 @@ class CleanCommand(Command):
                 'rm -vrf ./build ./dist ./*.pyc ./*tgz ./*.egg-info *.rst *.db *~'
             )
 
-setup(name = 'remarkable-tweak',
-      version = '0.1.0',
-      description = 'Tweak tool for the reMarkable paper tablet.',
-      long_description = readme(),
-      classifiers = [
-          'Development Status :: 1 - Planning',
-          'Intended Audience :: End Users/Desktop',
-          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-          'Programming Language :: Python :: 3',
-          'Topic :: Utilities'
-      ],
-      url = 'https://github.com/morngrar/remarkable-tweak',
-      author = 'Svein-Kåre Bjørnsen',
-      author_email = 'sveinkare@gmail.com',
-      license = 'GPL',
-      include_package_data = True,
-      packages = find_packages(),
-      install_requires = [
-          "paramiko>=2.4.0",
-          "Pillow>=5.0.0",
-      ],
-      entry_points = {
-          'gui_scripts': [
-              'remarkable-tweak = remarkable_tweak.__main__:main'
-          ]
-      },
-      cmdclass = {
-          'clean': CleanCommand,
-      },
-      zip_safe = False
+setup(
+    name='remarkable-tweak',
+    version='0.1.0',
+    description='Tweak tool for the reMarkable paper tablet.',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities'
+    ],
+    url='https://github.com/morngrar/remarkable-tweak',
+    author='Svein-Kåre Bjørnsen',
+    author_email='sveinkare@gmail.com',
+    license='GPL',
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[
+        "paramiko>=2.4.0",
+        "Pillow>=5.0.0",
+    ],
+    entry_points={
+        'gui_scripts': [
+            'remarkable-tweak = remarkable_tweak.__main__:main'
+        ]
+    },
+    cmdclass={
+        'clean': CleanCommand,
+    },
+    zip_safe=False
 )
